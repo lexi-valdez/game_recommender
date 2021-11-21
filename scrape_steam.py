bcf5272da4c9a27c223e992b81255fa383c15b8f
unknown <lexi.valdez24@gmail.com>
2021-11-20 20:56:16 -0600

    updated to scrape ~500 games

--- scrape_steam.py
+++ scrape_steam.py
@@ -29,12 +29,17 @@ def scrape_test():
 # Each app_id is used to access a URL for a specific game
 def get_app_ids():
     app_ids = []
-    url = 'https://store.steampowered.com/search/?term=' # this url shows all projects on Steam
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    url = 'https://store.steampowered.com/search/?page=' # this url is used to page through all games on Steam
+                                                            # follow url with a number to get a new page of games
 
-    for game in soup.find_all("a", {"class":"search_result_row ds_collapse_flag"}):
-        app_ids.append(game["data-ds-appid"])
+    max_pages = 20 # loop through pages 1 - 20 (~500 games total)
+    for i in range(1, max_pages + 1):
+        print(i)
+        r = requests.get(url + str(i)) 
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        for game in soup.find_all("a", {"class":"search_result_row ds_collapse_flag"}):
+            app_ids.append(game["data-ds-appid"])
 
     return app_ids
 
@@ -45,27 +50,39 @@ def get_game_data(url):
     soup = BeautifulSoup(r.text, 'html.parser')
     
     # get game title
-    title = soup.find(itemprop='name').get_text() 
-
+    title = soup.find(itemprop='name')
+    if title == None: # if title not found, set to empty string
+        title = ''
+    else: # else get title text
+        title = title.get_text()
+    
     # get genres
     genre_list = ['' for i in range(3)]
     i = 0
-    for genre in soup.find(id="genresAndManufacturer").find("span").find_all("a"):
-        if i == 3: # allowing three genres at most
-            break
-        genre_list[i] = genre.get_text()
-        i += 1
+
+    if soup.find(id="genresAndManufacturer") != None: # error checking if any NoneTypes are returned
+        if soup.find(id="genresAndManufacturer").find("span") != None:
+            genres = soup.find(id="genresAndManufacturer").find("span").find_all("a")
+
+            for genre in genres:
+                if i == 3: # allowing three genres at most
+                    break
+                genre_list[i] = genre.get_text()
+                i += 1
 
     # get user-defined tags (20 per game, ignore the 1st '' tag and the 21st '+' tag)
     tag_list = ['' for i in range(20)]
     i = 0
-    for tag in soup.find("div", {"class":"glance_tags popular_tags"}):
-        if i == 20: # only 20 tags maximum
-            break
-        tag_ = tag.get_text().strip()
-        if tag_ != '+' and tag_ != '':
-            tag_list[i] = tag_
-            i += 1
+
+    tags = soup.find("div", {"class":"glance_tags popular_tags"})
+    if tags != None:
+        for tag in tags:
+            if i == 20: # only 20 tags maximum
+                break
+            tag_ = tag.get_text().strip()
+            if tag_ != '+' and tag_ != '':
+                tag_list[i] = tag_
+                i += 1
 
     # get percentage of positive reviews and total user reviews
     review_desc = soup.find("span", {"class":"responsive_reviewdesc_short"})
@@ -102,6 +119,8 @@ def scrape_steam():
     
     for i in range(len(app_ids)): # loop through each app id
         url = 'https://store.steampowered.com/app/' + app_ids[i] # get url for each game
+
+        #print(url)
         data = get_game_data(url)
         game_dict[data[0]] = (data[1][0], data[1][1], data[1][2], 
                                 data[2][0], data[2][1], data[2][2], data[2][3], data[2][4], data[2][5], data[2][6], data[2][7], data[2][8], data[2][9], 
