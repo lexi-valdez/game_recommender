810e06e3d9fd3f23a193f9c69c2988f3144968da
Lexi Valdez <lexi.valdez24@gmail.com>
2021-12-25 16:07:37 -0600

    added user input/output functionality

--- recommend.py
+++ recommend.py
@@ -1,11 +1,7 @@
 # Recommend 3 games based on a given game title
 
 import pandas as pd
-
-# input_game() prompts user for a video game they want recommendations for
-def input_game():
-    title = 'Fallout 4'
-    return title
+import tkinter as tk
 
 # generate_lookups() creates two lookup dictionaries for the row index and video game title
 def generate_lookups():
@@ -20,24 +16,58 @@ def generate_lookups():
 
     return game_lookup, index_lookup
 
-# find_recommendations() takes an input game title and returns 3 games most similar to it
-def find_recommendations(title, game_lookup, index_lookup):
-    cos_sim = pd.read_excel('game_data.xlsx', sheet_name='Cosine Similarity')
-    index = index_lookup.get(title) # row corresponding to the input game title
-    score_list = []
+# handle_user() prompts user for a video game they want recommendations for and returns 3 similar games
+def handle_user():
+    root = tk.Tk()
+    canvas = tk.Canvas(root, width=400, height=240)
+    canvas.pack()
 
-    for i in range(len(cos_sim[index])): # loop through each column and create list of valid similarity scores
-        score = cos_sim[index][i]
-        if index != i: # ignore when similarity score = 1 (comparing game to itself)
-            score_list.append((score, i))
+    prompt = tk.Label(root, text='Enter a video game title:') # create prompt text
+    canvas.create_window(110, 30, window=prompt)
+
+    entry = tk.Entry(root) # create input text box
+    canvas.create_window(200, 60, width=300, window=entry)
+
+    # initialize results to display later
+    result1 = tk.Label(root, text='')
+    canvas.create_window(200, 130, window=result1)
+    result2 = tk.Label(root, text='')
+    canvas.create_window(200, 160, window=result2)
+    result3 = tk.Label(root, text='')
+    canvas.create_window(200, 190, window=result3)
+
+    # find_recommendations() generates 3 games using cosine similarity scores and prints them to screen
+    def find_recommendations():
+        game_lookup, index_lookup = generate_lookups()
+        title = entry.get() # get user input title
+
+        cos_sim = pd.read_excel('game_data.xlsx', sheet_name='Cosine Similarity')
+        index = index_lookup.get(title) # row corresponding to the input game title
+
+        if index == None: # if game does not exist in database, use default values
+            game1, game2, game3 = 'None', 'None', 'None' 
+        else: # otherwise, find 3 most similar games
+            score_list = []
+            for i in range(len(cos_sim[index])): # loop through each column and create list of valid similarity scores
+                score = cos_sim[index][i]
+                if index != i: # ignore when similarity score = 1 (comparing game to itself)
+                    score_list.append((score, i))
+        
+            score_list.sort(reverse=True) # sort list to find highest 3 scores
+
+            game1 = game_lookup.get(score_list[0][1]) # lookup first game by its index
+            game2 = game_lookup.get(score_list[1][1]) # lookup second game by its index
+            game3 = game_lookup.get(score_list[2][1]) # lookup third game by its index
+
+        # show results on screen
+        result1.config(text='Game 1: ' + game1)
+        result2.config(text='Game 2: ' + game2)
+        result3.config(text='Game 3: ' + game3)
+
+    button = tk.Button(text='Enter', command=find_recommendations)
+    canvas.create_window(200, 90, window=button)
     
-    score_list.sort(reverse=True) # sort list to find highest 3 scores
-    
-    for i in range(3):
-        game = game_lookup.get(score_list[i][1]) # lookup game by its index
-        print(game)
+    root.mainloop()
 
 if __name__ == "__main__":
-    game_lookup, index_lookup = generate_lookups()
-    title = input_game()
-    find_recommendations(title, game_lookup, index_lookup)
+    handle_user()
